e2d2969d6f273e6ef5fa8a3e6627af64c9e74656
merve çalışır <m-r-v-c-96@hotmail.com>
2021-02-11 00:00:43 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        internal static string MaintenanceTime = "Sistem bakımda";
+        internal static string ProductsListed = "Ürünler listelemdi";
+    }
+}
---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    //IEntity implement 
+    public interface IEntity
+
+    {
+
+    }
+}
